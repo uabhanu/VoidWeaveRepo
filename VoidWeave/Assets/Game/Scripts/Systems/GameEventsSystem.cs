52a9c13bc387bc1f6475af7b6d8428501a8f0ddc
Agasthyeaswar Bhanu Upadhyayula <uabhanu@gmail.com>
2026-02-11 16:04:17 +0000

    Feature: Implement Ownership-based Turret Cooldown UI
    - GameEventsSystem.cs: Implement OnTurretCooldownStarted event with tag filtering for specific turrets
    - InGameUI.cs: Implement _trackingEntity logic to lock the timer label to a single turret during deployment
    - InGameUI.uxml: Add TimerUIVisualElement with Label hierarchy for world-space cooldown display

--- VoidWeave/Assets/Game/Scripts/Systems/GameEventsSystem.cs
+++ VoidWeave/Assets/Game/Scripts/Systems/GameEventsSystem.cs
@@ -3,29 +3,25 @@ namespace Game.Scripts.Systems
     using Components;
     using System;
     using Unity.Entities;
+    using Unity.Mathematics;
+    using Unity.Transforms;
 
     public partial class GameEventsSystem : SystemBase
     {
         public static event Action<float> OnEnergyValueChanged;
         public static event Action<float> OnHealthValueChanged;
         public static event Action<int> OnLevelValueChanged;
+        public static event Action<Entity , float , float3> OnTurretCooldownStarted;
 
         protected override void OnUpdate()
         {
-            foreach(var currentEnergy in SystemAPI.Query<RefRO<CurrentEnergyComponent>>().WithChangeFilter<CurrentEnergyComponent>())
-            {
-                OnEnergyValueChanged?.Invoke(currentEnergy.ValueRO.Value);
-            }
-            
-            foreach(var currentHealth in SystemAPI.Query<RefRO<CurrentHealthComponent>>().WithAll<PlayerTag>().WithChangeFilter<CurrentHealthComponent>())
-            {
-                OnHealthValueChanged?.Invoke(currentHealth.ValueRO.Value);
-            }
-            
-            foreach(var currentLevel in SystemAPI.Query<RefRO<LevelComponent>>().WithChangeFilter<LevelComponent>())
-            {
-                OnLevelValueChanged?.Invoke(currentLevel.ValueRO.Value);
-            }
+            foreach(var currentEnergyComponent in SystemAPI.Query<RefRO<CurrentEnergyComponent>>().WithChangeFilter<CurrentEnergyComponent>()) { OnEnergyValueChanged?.Invoke(currentEnergyComponent.ValueRO.Value); }
+
+            foreach(var currentHealthComponent in SystemAPI.Query<RefRO<CurrentHealthComponent>>().WithAll<PlayerTag>().WithChangeFilter<CurrentHealthComponent>()) { OnHealthValueChanged?.Invoke(currentHealthComponent.ValueRO.Value); }
+
+            foreach(var levelComponent in SystemAPI.Query<RefRO<LevelComponent>>().WithChangeFilter<LevelComponent>()) { OnLevelValueChanged?.Invoke(levelComponent.ValueRO.Value); }
+
+            foreach(var (cooldownComponent , transform , turretEntity) in SystemAPI.Query<RefRO<CooldownComponent> , RefRO<LocalTransform>>().WithAny<BeamTurretTag , ScatterTurretTag , StrikerTurretTag>().WithChangeFilter<CooldownComponent>().WithEntityAccess()) { OnTurretCooldownStarted?.Invoke(turretEntity , cooldownComponent.ValueRO.Value , transform.ValueRO.Position); }
         }
     }
 }
--- VoidWeave/Assets/Game/Scripts/UI/InGameUI.cs
+++ VoidWeave/Assets/Game/Scripts/UI/InGameUI.cs
@@ -3,12 +3,15 @@ namespace Game.Scripts.UI
     using Components;
     using Systems;
     using Unity.Entities;
+    using Unity.Mathematics;
     using UnityEngine;
     using UnityEngine.UIElements;
 
     public class InGameUI : MonoBehaviour
     {
         #region Variables
+
+        private Entity _trackingEntity = Entity.Null;
 
         private EntityQuery _energyQuery;
         private EntityQuery _healthQuery;
@@ -22,10 +25,17 @@ namespace Game.Scripts.UI
         private Label _healthValueLabel;
         private Label _levelTextLabel;
         private Label _levelValueLabel;
+        private Label _timerTextLabel;
+        private Label _timerValueLabel;
+
+        private Camera _mainCamera;
+
+        private VisualElement _rootVisualElement;
 
         [SerializeField] private Color energyLabelColor = Color.yellow;
         [SerializeField] private Color healthLabelColor = Color.green;
         [SerializeField] private Color levelLabelColor = Color.orange;
+        [SerializeField] private Color timerLabelColor = Color.red;
 
         [SerializeField] private UIDocument uiDocument;
 
@@ -37,6 +47,8 @@ namespace Game.Scripts.UI
         {
             if(!uiDocument) uiDocument = GetComponent<UIDocument>();
 
+            _mainCamera = Camera.main;
+
             var world = World.DefaultGameObjectInjectionWorld;
             _entityManager = world.EntityManager;
 
@@ -44,16 +56,19 @@ namespace Game.Scripts.UI
             _healthQuery = _entityManager.CreateEntityQuery(typeof(CurrentHealthComponent) , typeof(PlayerTag));
             _levelQuery = _entityManager.CreateEntityQuery(typeof(LevelComponent));
 
-            var rootVisualElement = uiDocument.rootVisualElement;
+            _rootVisualElement = uiDocument.rootVisualElement;
 
-            _energyTextLabel = rootVisualElement.Q<Label>("EnergyTextLabel");
-            _energyValueLabel = rootVisualElement.Q<Label>("EnergyValueLabel");
+            _energyTextLabel = _rootVisualElement.Q<Label>("EnergyTextLabel");
+            _energyValueLabel = _rootVisualElement.Q<Label>("EnergyValueLabel");
 
-            _healthTextLabel = rootVisualElement.Q<Label>("HealthTextLabel");
-            _healthValueLabel = rootVisualElement.Q<Label>("HealthValueLabel");
+            _healthTextLabel = _rootVisualElement.Q<Label>("HealthTextLabel");
+            _healthValueLabel = _rootVisualElement.Q<Label>("HealthValueLabel");
 
-            _levelTextLabel = rootVisualElement.Q<Label>("LevelTextLabel");
-            _levelValueLabel = rootVisualElement.Q<Label>("LevelValueLabel");
+            _levelTextLabel = _rootVisualElement.Q<Label>("LevelTextLabel");
+            _levelValueLabel = _rootVisualElement.Q<Label>("LevelValueLabel");
+
+            _timerTextLabel = _rootVisualElement.Q<Label>("TimerTextLabel");
+            _timerValueLabel = _rootVisualElement.Q<Label>("TimerValueLabel");
 
             _energyTextLabel.style.backgroundColor = energyLabelColor;
             _energyValueLabel.style.backgroundColor = energyLabelColor;
@@ -70,49 +85,72 @@ namespace Game.Scripts.UI
             GameEventsSystem.OnEnergyValueChanged += OnEnergyValueChanged;
             GameEventsSystem.OnHealthValueChanged += OnHealthValueChanged;
             GameEventsSystem.OnLevelValueChanged += OnLevelValueChanged;
+            GameEventsSystem.OnTurretCooldownStarted += OnTurretCooldownStarted;
         }
-        
+
         private void OnDisable()
         {
             GameEventsSystem.OnEnergyValueChanged -= OnEnergyValueChanged;
             GameEventsSystem.OnHealthValueChanged -= OnHealthValueChanged;
             GameEventsSystem.OnLevelValueChanged -= OnLevelValueChanged;
+            GameEventsSystem.OnTurretCooldownStarted -= OnTurretCooldownStarted;
         }
-        
+
         #endregion
-        
+
         #region Event Callbacks
 
         private void OnEnergyValueChanged(float currentEnergy)
         {
             _entityManager.CompleteDependencyBeforeRO<CurrentEnergyComponent>();
-            
-            if(!_energyQuery.IsEmptyIgnoreFilter)
-            {
-                _energyValueLabel.text = $"{currentEnergy:F0}";
-            }
+
+            if(!_energyQuery.IsEmptyIgnoreFilter) { _energyValueLabel.text = $"{currentEnergy:F0}"; }
         }
-        
+
         private void OnHealthValueChanged(float currentHealth)
         {
             _entityManager.CompleteDependencyBeforeRO<CurrentHealthComponent>();
-            
-            if(!_healthQuery.IsEmptyIgnoreFilter)
-            {
-                _healthValueLabel.text = $"{currentHealth:F0}";
-            }
+
+            if(!_healthQuery.IsEmptyIgnoreFilter) { _healthValueLabel.text = $"{currentHealth:F0}"; }
         }
-        
+
         private void OnLevelValueChanged(int currentLevel)
         {
             _entityManager.CompleteDependencyBeforeRO<LevelComponent>();
-            
-            if(!_levelQuery.IsEmptyIgnoreFilter)
-            {
-                _levelValueLabel.text = $"{currentLevel:F0}";
-            }
+
+            if(!_levelQuery.IsEmptyIgnoreFilter) { _levelValueLabel.text = $"{currentLevel:F0}"; }
         }
-        
+
+        private void OnTurretCooldownStarted(Entity entity , float timer , float3 worldPosition)
+        {
+            if(timer <= 0)
+            {
+                if(_trackingEntity == entity)
+                {
+                    _timerValueLabel.style.display = DisplayStyle.None;
+                    _timerTextLabel.style.display = DisplayStyle.None;
+                    _trackingEntity = Entity.Null;
+                }
+
+                return;
+            }
+
+            if(timer > 1.0f) { _trackingEntity = entity; }
+            
+            if(_trackingEntity != entity) return;
+            
+            _timerValueLabel.style.display = DisplayStyle.Flex;
+            _timerValueLabel.text = $"{timer:F1}";
+            _timerTextLabel.style.display = DisplayStyle.Flex;
+
+            Vector2 screenPos = RuntimePanelUtils.CameraTransformWorldToPanel(_rootVisualElement.panel , worldPosition , _mainCamera);
+
+            _timerValueLabel.style.left = screenPos.x - 25;
+            _timerValueLabel.style.top = screenPos.y - 50;
+            _timerTextLabel.style.left = screenPos.x - 25;
+            _timerTextLabel.style.top = screenPos.y - 70;
+        }
+
         #endregion
     }
 }
