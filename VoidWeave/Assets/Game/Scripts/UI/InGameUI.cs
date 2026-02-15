13617f21b9f383a3e7b7191e5044654ff3eab23b
Agasthyeaswar Bhanu Upadhyayula <uabhanu@gmail.com>
2026-02-15 18:53:54 +0000

    Feature: Implement dynamic Wave Prep Label with serialized styling and anchor-based centering - Game.unity: Updated InGameUI component with serialized styling values - InGameUI.cs: Replaced static timerLabel with dynamic wavePrepLabel logic - InGameUI.uxml: Removed static TimerVisualElement to support dynamic instantiation - Added anchor-based centering using timerLabelAnchorPercent

--- VoidWeave/Assets/Game/Scripts/UI/InGameUI.cs
+++ VoidWeave/Assets/Game/Scripts/UI/InGameUI.cs
@@ -15,8 +15,6 @@ namespace Game.Scripts.UI
         private Button _restartButton;
         private Button _resumeButton;
 
-        private Entity _trackingEntity = Entity.Null;
-
         private EntityQuery _energyQuery;
         private EntityQuery _healthQuery;
         private EntityQuery _levelQuery;
@@ -29,17 +27,32 @@ namespace Game.Scripts.UI
         private Label _healthValueLabel;
         private Label _levelTextLabel;
         private Label _levelValueLabel;
-        private Label _timerLabel;
-
-        private Camera _mainCamera;
+        private Label _wavePrepLabel;
 
         private VisualElement _rootVisualElement;
         private VisualElement _pauseMenuVisualElement;
 
-        [SerializeField] private Color energyLabelColor = Color.yellow;
-        [SerializeField] private Color healthLabelColor = Color.green;
-        [SerializeField] private Color levelLabelColor = Color.orange;
-        [SerializeField] private Color timerLabelColor = Color.red;
+        [SerializeField] private Color energyLabelColor;
+        [SerializeField] private Color healthLabelColor;
+        [SerializeField] private Color levelLabelColor;
+        [SerializeField] private Color timerLabelBorderColor;
+        [SerializeField] private Color timerLabelColor;
+
+        [SerializeField] private float timerLabelAnchorPercent;
+        [SerializeField] private float timerLabelBorderWidth;
+        [SerializeField] private float timerLabelPadding;
+        [SerializeField] private float timerLabelFontSize;
+        [SerializeField] private float timerLabelHeight;
+        [SerializeField] private float timerLabelOffsetX;
+        [SerializeField] private float timerLabelOffsetY;
+        [SerializeField] private float timerLabelTranslatePercentX;
+        [SerializeField] private float timerLabelTranslatePercentY;
+        [SerializeField] private float timerLabelTranslatePercentZ;
+        [SerializeField] private float timerLabelWidth;
+        [SerializeField] private float turretCooldownThreshold;
+        [SerializeField] private float zeroThreshold;
+
+        [SerializeField] private int waveStateReadyValue;
 
         [SerializeField] private UIDocument uiDocument;
 
@@ -51,8 +64,6 @@ namespace Game.Scripts.UI
         {
             if(!uiDocument) uiDocument = GetComponent<UIDocument>();
 
-            _mainCamera = Camera.main;
-
             var world = World.DefaultGameObjectInjectionWorld;
             _entityManager = world.EntityManager;
 
@@ -61,11 +72,11 @@ namespace Game.Scripts.UI
             _levelQuery = _entityManager.CreateEntityQuery(typeof(LevelComponent));
 
             _rootVisualElement = uiDocument.rootVisualElement;
-            
+
             _pauseButton = _rootVisualElement.Q<Button>("PauseButton");
             _restartButton = _rootVisualElement.Q<Button>("RestartButton");
             _resumeButton = _rootVisualElement.Q<Button>("ResumeButton");
-            
+
             _pauseButton.clicked += () => { GameEventsSystem.OnPauseButtonClicked?.Invoke(); };
             _restartButton.clicked += () => { GameEventsSystem.OnRestartButtonClicked?.Invoke(); };
             _resumeButton.clicked += () => { GameEventsSystem.OnResumeButtonClicked?.Invoke(); };
@@ -79,8 +90,6 @@ namespace Game.Scripts.UI
             _levelTextLabel = _rootVisualElement.Q<Label>("LevelTextLabel");
             _levelValueLabel = _rootVisualElement.Q<Label>("LevelValueLabel");
 
-            _timerLabel = _rootVisualElement.Q<Label>("TimerLabel");
-
             _pauseMenuVisualElement = _rootVisualElement.Q<VisualElement>("PauseMenuVisualElement");
             _pauseMenuVisualElement.style.display = DisplayStyle.None;
 
@@ -92,8 +101,6 @@ namespace Game.Scripts.UI
 
             _levelTextLabel.style.backgroundColor = levelLabelColor;
             _levelValueLabel.style.backgroundColor = levelLabelColor;
-
-            _timerLabel.style.backgroundColor = timerLabelColor;
         }
 
         private void OnEnable()
@@ -121,9 +128,9 @@ namespace Game.Scripts.UI
         }
 
         #endregion
-        
+
         #region Button Event Callbacks
-        
+
         private void OnPauseButtonClicked()
         {
             _entityManager.CreateEntity(typeof(PauseInputTag));
@@ -138,11 +145,8 @@ namespace Game.Scripts.UI
             _pauseMenuVisualElement.style.display = DisplayStyle.None;
         }
 
-        private void OnRestartButtonClicked()
-        {
-            _entityManager.CreateEntity(typeof(RestartInputTag));
-        }
-        
+        private void OnRestartButtonClicked() { _entityManager.CreateEntity(typeof(RestartInputTag)); }
+
         #endregion
 
         #region Event Callbacks
@@ -168,45 +172,56 @@ namespace Game.Scripts.UI
             if(!_levelQuery.IsEmptyIgnoreFilter) { _levelValueLabel.text = $"{currentLevel:F0}"; }
         }
 
-        private void OnTurretCooldownStarted(Entity entity , float timer , float3 worldPosition)
+        private void OnTurretCooldownStarted(Entity entity , float timer , float3 worldPosition) {}
+
+        private void OnWavePrepCountdownStarted(float timer , int waveState)
         {
-            if(timer <= 0)
+            if(waveState != waveStateReadyValue || timer <= zeroThreshold)
             {
-                if(_trackingEntity == entity)
+                if(_wavePrepLabel != null)
                 {
-                    _timerLabel.style.display = DisplayStyle.None;
-                    _trackingEntity = Entity.Null;
+                    _rootVisualElement.Remove(_wavePrepLabel);
+                    _wavePrepLabel = null;
                 }
 
                 return;
             }
 
-            if(timer > 1.0f) { _trackingEntity = entity; }
-
-            if(_trackingEntity != entity) return;
-
-            _timerLabel.style.display = DisplayStyle.Flex;
-            _timerLabel.text = $"{timer:F1}";
-
-            Vector2 screenPos = RuntimePanelUtils.CameraTransformWorldToPanel(_rootVisualElement.panel , worldPosition , _mainCamera);
-
-            _timerLabel.style.left = screenPos.x - 25;
-            _timerLabel.style.top = screenPos.y - 50;
-        }
-
-        private void OnWavePrepCountdownStarted(float timer , int waveState)
-        {
-            if(waveState != 0 || timer <= 0)
+            if(_wavePrepLabel == null)
             {
-                _timerLabel.style.display = DisplayStyle.None;
-                _timerLabel.text = string.Empty;
-                return;
+                _wavePrepLabel = new Label
+                {
+                    style =
+                    {
+                        backgroundColor = timerLabelColor ,
+                        borderBottomColor = timerLabelBorderColor ,
+                        borderBottomWidth = timerLabelBorderWidth ,
+                        borderLeftColor = timerLabelBorderColor ,
+                        borderLeftWidth = timerLabelBorderWidth ,
+                        borderRightColor = timerLabelBorderColor ,
+                        borderRightWidth = timerLabelBorderWidth ,
+                        borderTopColor = timerLabelBorderColor ,
+                        borderTopWidth = timerLabelBorderWidth ,
+                        fontSize = timerLabelFontSize ,
+                        height = timerLabelHeight ,
+                        left = Length.Percent(timerLabelAnchorPercent) ,
+                        paddingBottom = timerLabelPadding ,
+                        paddingLeft = timerLabelPadding ,
+                        paddingRight = timerLabelPadding ,
+                        paddingTop = timerLabelPadding ,
+                        position = Position.Absolute ,
+                        top = Length.Percent(timerLabelAnchorPercent) ,
+                        translate = new Translate(Length.Percent(timerLabelTranslatePercentX) , Length.Percent(timerLabelTranslatePercentY) , timerLabelTranslatePercentZ) ,
+                        unityFontStyleAndWeight = FontStyle.Bold ,
+                        unityTextAlign = TextAnchor.MiddleCenter ,
+                        width = timerLabelWidth
+                    }
+                };
+
+                _rootVisualElement.Add(_wavePrepLabel);
             }
 
-            _timerLabel.style.display = DisplayStyle.Flex;
-            _timerLabel.text = $"Next Wave In\n{timer:F0}";
-
-            _timerLabel.style.translate = new Translate(Length.Percent(120) , Length.Percent(200) , 0);
+            _wavePrepLabel.text = $"Next Wave In\n{timer:F0}";
         }
 
         #endregion
