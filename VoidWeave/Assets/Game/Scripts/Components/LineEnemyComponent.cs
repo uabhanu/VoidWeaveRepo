af6e23dd8b4fc0ec0dfa6b66fd022b97a5102f3e
Agasthyeaswar Bhanu Upadhyayula <uabhanu@gmail.com>
2026-03-12 16:33:36 +0000

    Refactor : Added Comments to identiy these scripts

--- VoidWeave/Assets/Game/Scripts/Components/LineEnemyComponent.cs
+++ VoidWeave/Assets/Game/Scripts/Components/LineEnemyComponent.cs
@@ -2,6 +2,7 @@ namespace Game.Scripts.Components
 {
     using Unity.Entities;
 
+    //This is to identify the Melee Enemy as Line Enemy
     public struct LineEnemyComponent : IComponentData
     {
         public int Value;
--- VoidWeave/Assets/Game/Scripts/Components/TriangleEnemyComponent.cs
+++ VoidWeave/Assets/Game/Scripts/Components/TriangleEnemyComponent.cs
@@ -2,6 +2,7 @@ namespace Game.Scripts.Components
 {
     using Unity.Entities;
 
+    //This is to identify the Melee Enemy as Triangle Enemy
     public struct TriangleEnemyComponent : IComponentData
     {
         public int Value;
